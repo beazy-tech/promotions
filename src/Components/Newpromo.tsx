a89b89c7030f9582920127855bedb37d3714f319
Harit Sharma <haritsharma058@gmail.com>
2023-05-29 13:22:45 +0530

    some fixes 29/05/2023

--- src/Components/Newpromo.tsx
+++ src/Components/Newpromo.tsx
@@ -116,7 +116,7 @@ export default function Createpromo() {
     if (isPromotionCreated) {
       route.push("/mypromos")
     }
-  }, [isPromotionCreated])
+  }, [route,isPromotionCreated])
   useEffect(()=>{
     if(tosterData.showPopUp)
     {
@@ -135,13 +135,13 @@ export default function Createpromo() {
             {state.category!=="Brand promotion"?
             <>
               <SelectionInput Label="Promotion Type" Name="Promotion_type" types={promtionType} dispatch={dispatch} State={state} />
-              {state.category!=="Custom message"?state.category!=="_ days free trial"?state?.category !== "Rs_ off on purchases above Rs_" ?
-                <Input required={true} Lable="Discount" Type="number" Placeholder={state?.category === "Flat Rs_ Off" ? "Discount(in Rs)" : state?.category === "Flat_ % discount" ? "Discount(in %)" : "Discount Price"} dispatch={dispatch} value={state.value} /> :
+              {state.category.length>0?state.category!=="Custom message"?state.category!=="_ days free trial"?state?.category !== "Rs_ off on purchases above Rs_" ?
+                <Input required={true} Lable="Discount" Type="number" Placeholder={state?.category === "Flat Rs_ Off" ? "Discount(in Rs)" : state?.category === "Flat _% discount" ? "Discount(in %)" : "Discount Price"} dispatch={dispatch} value={state.value} /> :
                 <div className={styles.discount_off_onn}>
                   <Input required={true} Lable="Discount (in Rs)" Type="number" Placeholder={"discount of (in rs)"} dispatch={dispatch} value={state.value} />
                   <Input required={true} Lable="Above (in Rs)" Type="number" Placeholder={"Above (in rs)"} dispatch={dispatch} value={state.value} />
                 </div>:
-                <Input required={true} Lable="Number of Days" Type="number" Placeholder={"type here"} dispatch={dispatch} value={state.value} />:<MessageInput dispatch={dispatch} placeholder='Enter your Message (less then 200 characters)' messageValue={state.message}/>
+                <Input required={true} Lable="Number of Days" Type="number" Placeholder={"type here"} dispatch={dispatch} value={state.value} />:<MessageInput dispatch={dispatch} placeholder='Enter your Message (less then 200 characters)' messageValue={state.message}/>:<></>
               }
             </>:
             <MessageInput dispatch={dispatch} placeholder='Enter your Message (less then 50 characters only)' messageValue={state.message}/>  }
