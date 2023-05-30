e61b3865423c89478a5c2d007f54f70011312a44
Harit Sharma <haritsharma058@gmail.com>
2023-05-30 11:33:28 +0530

    updated

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={'anonymous'} />
+          <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@700&family=Inter:wght@400;500&family=Open+Sans:wght@500&family=Roboto:wght@400;500&display=swap" rel="stylesheet"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Index from '@/Components/Index'
-import Toster from '@/Components/Toster/Toster'
 export default function Home() {
   return (
     <>
